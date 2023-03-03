5688fc20ffe2e8d19223a0d4072aa58aac3967c0
Jay Jackson <jj@jj.is-a.dev>
2023-03-03 16:43:49 -0500

    better movement

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use bevy::prelude::*;
-use smooth_bevy_cameras::{LookTransform, LookTransformBundle, LookTransformPlugin, Smoother};
-use tracing::info;
 
 fn main() {
     let mut app = App::new();
 
-    app.insert_resource(ClearColor(Color::rgb(0., 0., 0.)))
+    app.insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             window: {
                 WindowDescriptor {
@@ -30,7 +28,7 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut framepace_settings: ResMut<bevy_framepace::FramepaceSettings>,
 ) {
-    framepace_settings.limiter = bevy_framepace::Limiter::from_framerate(60.);
+    framepace_settings.limiter = bevy_framepace::Limiter::from_framerate(60.0);
 
     // plane
     commands.spawn(PbrBundle {
@@ -56,26 +54,34 @@ fn setup(
         ..default()
     });
     // camera
-    commands.spawn(LookTransformBundle {
-        transform: LookTransform::new(Vec3::ZERO, Vec3::Y),
-        smoother: Smoother::new(0.9), // Value between 0.0 and 1.0, higher is smoother.
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-1.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
     });
-    commands.spawn(Camera3dBundle::default());
 }
 
-fn move_camera(input: Res<Input<KeyCode>>, mut cams: Query<&mut LookTransform>) {
-    let (mut x, mut y, mut z) = (0., 0., 0.);
+fn move_camera(input: Res<Input<KeyCode>>, mut cams: Query<&mut Transform, With<Camera3d>>) {
+    let (mut x, mut y, mut z) = (0.0, 0.0, 0.0);
+
     if input.pressed(KeyCode::W) || input.pressed(KeyCode::Up) {
-        y += 0.5;
-    } else if input.pressed(KeyCode::S) || input.pressed(KeyCode::Down) {
-        y -= 0.5;
-    } else if input.pressed(KeyCode::D) || input.pressed(KeyCode::Right) {
+        z -= 0.5;
+    }
+    if input.pressed(KeyCode::S) || input.pressed(KeyCode::Down) {
+        z += 0.5;
+    }
+    if input.pressed(KeyCode::D) || input.pressed(KeyCode::Right) {
         x += 0.5;
-    } else if input.pressed(KeyCode::A) || input.pressed(KeyCode::Right) {
+    }
+    if input.pressed(KeyCode::A) || input.pressed(KeyCode::Right) {
         x -= 0.5;
-    } else {
-        return;
     };
+
+    if input.pressed(KeyCode::LShift) {
+        y += 0.5;
+    } else if input.pressed(KeyCode::LControl) {
+        y -= 0.5;
+    }
+
     let mv = Vec3::new(x, y, z);
 
     for mut t in &mut cams {
