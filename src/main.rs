7648fd55623102ed0b2cfe1f58e40b28ffbefb46
Jay Jackson <jj@jj.is-a.dev>
2023-03-06 18:50:59 -0500

    better initlial camera

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@
 use std::f32::consts::FRAC_PI_2;
 
 use bevy::{
-    core_pipeline::fxaa::{Fxaa, Sensitivity},
+    core_pipeline::{
+        bloom::BloomSettings,
+        fxaa::{Fxaa, Sensitivity},
+    },
     prelude::*,
 };
-use bevy_mod_outline::{OutlineBundle, OutlinePlugin, OutlineVolume};
 use bevy_mod_picking::{
     InteractablePickingPlugin, PickableBundle, PickingCameraBundle, PickingEvent, PickingPlugin,
     SelectionEvent,
@@ -19,19 +21,16 @@ mod planets;
 #[derive(Component)]
 struct CurrentPlanet;
 
-#[derive(Default, Debug, Clone, Copy, PartialEq, Reflect, Resource)]
-struct Movement(Transform);
-
 #[bevy_main]
 fn main() {
     let mut app = App::new();
 
     app.insert_resource(ClearColor(Color::BLACK))
-        .insert_resource(Movement::default())
         .insert_resource(AmbientLight {
             brightness: 0.5, // represents the brightness of stars around the solar system
             ..Default::default()
         })
+        .add_event::<MovementEvent>()
         .add_event::<EscapeEvent>();
 
     #[cfg(target_arch = "wasm32")]
@@ -50,55 +49,64 @@ fn main() {
     }));
 
     app.add_plugin(LookTransformPlugin)
-        .add_plugin(bevy_framepace::FramepacePlugin)
-        .add_plugin(OutlinePlugin)
         .add_plugin(PickingPlugin)
         .add_plugin(InteractablePickingPlugin);
 
     app.add_startup_system(setup);
 
-    app.add_system(planet_selected)
+    app.add_system(movement_event)
+        .add_system(planet_selected)
         .add_system(planet_orbit)
         .add_system(lock_to_planet.after(planet_selected).after(planet_orbit))
         .add_system(escape)
         .add_system(escape_event);
 
+    #[cfg(debug_assertions)]
+    app.add_plugin(bevy_editor_pls::prelude::EditorPlugin);
+
     app.run()
 }
+
+#[derive(Component)]
+struct MainCamera;
 
 fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    mut framepace_settings: ResMut<bevy_framepace::FramepaceSettings>,
     mut escape: EventWriter<EscapeEvent>,
 ) {
-    framepace_settings.limiter = bevy_framepace::Limiter::from_framerate(60.0);
-
     commands
         .spawn(LookTransformBundle {
-            transform: LookTransform {
-                eye: Vec3::ONE,
-                target: Vec3::ZERO,
-                up: Vec3::Y,
-            },
-            smoother: Smoother::new(0.9),
+            smoother: Smoother::new(0.8),
+            transform: LookTransform::new(Vec3::ONE, Vec3::ZERO, Vec3::Y),
         })
         .insert((
-            Camera3dBundle::default(),
+            Camera3dBundle {
+                camera: Camera {
+                    hdr: true,
+                    ..default()
+                },
+                ..default()
+            },
+            BloomSettings::default(),
             PickingCameraBundle::default(), // <- Sets the camera to use for picking.
             Fxaa {
                 edge_threshold: Sensitivity::High,
                 ..default()
             },
-        ));
+        ))
+        .insert(MainCamera);
 
     escape.send(EscapeEvent);
 
     // planets
     macro_rules! planet {
-        ($name:ident) => {{
+        ($name:ident) => {
+            planet!($name, StandardMaterial::default(), t)
+        };
+        ($name:ident, $material:expr, $texture:ident) => {{
             let planet = Planet::$name;
             let mesh = Mesh::from(shape::UVSphere {
                 radius: planet.scaled_radius(),
@@ -106,18 +114,18 @@ fn setup(
                 stacks: 64,
             });
 
+            let $texture =
+                asset_server.load(format!("planets/{}.jpg", stringify!($name).to_lowercase()));
+
             let mut planet_id = commands.spawn((
                 PbrBundle {
                     mesh: meshes.add(mesh),
                     material: materials.add(StandardMaterial {
-                        base_color_texture: Some(
-                            asset_server
-                                .load(format!("planets/{}.jpg", stringify!($name).to_lowercase())),
-                        ),
+                        base_color_texture: Some($texture.clone()),
                         // not reflective
                         reflectance: 0.0,
                         metallic: 0.0,
-                        ..default()
+                        ..$material
                     }),
                     transform: {
                         let mut t = Transform::from_xyz(planet.scaled_distance(), 0.0, 0.0);
@@ -129,21 +137,21 @@ fn setup(
                 },
                 PickableBundle::default(), // <- Makes the mesh pickable.
             ));
-            planet_id
-                .insert(OutlineBundle {
-                    outline: OutlineVolume {
-                        visible: true,
-                        colour: Color::WHITE,
-                        width: 0.5,
-                    },
-                    ..default()
-                })
-                .insert(planet);
+            planet_id.insert(planet);
             planet_id
         }};
     }
 
-    planet!(Sun).with_children(|children| {
+    planet!(
+        Sun,
+        StandardMaterial {
+            emissive: Color::rgb_linear(255.0, 255.0, 255.0),
+            emissive_texture: Some(texture),
+            ..default()
+        },
+        texture
+    )
+    .with_children(|children| {
         children.spawn(PointLightBundle {
             point_light: PointLight {
                 color: Color::rgb_linear(250.0, 250.0, 250.0),
@@ -186,36 +194,31 @@ fn escape(kbd: ResMut<Input<KeyCode>>, mut events: EventWriter<EscapeEvent>) {
 fn planet_selected(
     mut commands: Commands,
     mut events: EventReader<PickingEvent>,
-    mut cameras: Query<&mut LookTransform, With<Camera3d>>,
-    planets: Query<(&Planet, &Transform)>,
     current_planet: Query<Entity, With<CurrentPlanet>>,
 ) {
     for event in events.iter() {
         if let PickingEvent::Selection(SelectionEvent::JustSelected(entity)) = event {
-            if let Ok((planet, transform)) = planets.get(*entity) {
-                info!("Selected planet: {:?}", planet);
-                if let Ok(planet) = current_planet.get_single() {
-                    commands.entity(planet).remove::<CurrentPlanet>();
-                }
-                commands.entity(*entity).insert(CurrentPlanet);
-                let mut camera = cameras.single_mut();
-                camera.target = transform.translation;
-                camera.eye =
-                    transform.translation + Vec3::new(planet.scaled_radius() * 3.0, 6.0, 0.0);
+            info!(?entity, "Selected planet");
+            if let Ok(planet) = current_planet.get_single() {
+                commands.entity(planet).remove::<CurrentPlanet>();
             }
+            commands.entity(*entity).insert(CurrentPlanet);
         }
     }
 }
 
 fn lock_to_planet(
     planet: Query<(&Planet, &Transform), With<CurrentPlanet>>,
-    mut cameras: Query<&mut LookTransform, With<Camera3d>>,
+    mut movement: EventWriter<MovementEvent>,
 ) {
-    if let Ok((planet, transform)) = planet.get_single() {
-        info!("Locking to planet: {:?}", planet);
-        let mut camera = cameras.single_mut();
-        camera.target = transform.translation;
-        camera.eye = transform.translation + Vec3::new(planet.scaled_radius() * 3.0, 6.0, 0.0);
+    if let Ok((planet, Transform { translation, .. })) = planet.get_single() {
+        info!(?planet, "Locking to planet");
+
+        movement.send(MovementEvent(LookTransform::new(
+            *translation - Vec3::new(0.0, 0.0, planet.scaled_radius() * 3.0),
+            *translation,
+            Vec3::Y,
+        )));
     }
 }
 
@@ -225,7 +228,7 @@ struct EscapeEvent;
 fn escape_event(
     mut commands: Commands,
     events: EventReader<EscapeEvent>,
-    mut cameras: Query<&mut LookTransform, With<Camera3d>>,
+    mut movement: EventWriter<MovementEvent>,
     mut planet: Query<Entity, With<CurrentPlanet>>,
 ) {
     if events.is_empty() {
@@ -233,11 +236,30 @@ fn escape_event(
     }
 
     events.clear();
-    let mut camera = cameras.single_mut();
-    camera.target = Vec3::ZERO;
-    camera.eye = Vec3::new(0.0, 1_000_000.0, 0.0);
+
+    movement.send(MovementEvent(LookTransform::new(
+        Vec3::new(0.0, 100_000.0, 0.0),
+        Vec3::ZERO,
+        Vec3::Y,
+    )));
+
     info!("Camera reset");
     if let Ok(planet) = planet.get_single_mut() {
         commands.entity(planet).remove::<CurrentPlanet>();
+    }
+}
+
+struct MovementEvent(LookTransform);
+
+fn movement_event(
+    mut events: EventReader<MovementEvent>,
+    mut cameras: Query<&mut LookTransform, With<MainCamera>>,
+) {
+    if let Some(event) = events.iter().last() {
+        let mut camera = cameras.single_mut();
+
+        *camera = event.0;
+
+        info!(translate = ?event.0, "Camera moved");
     }
 }
